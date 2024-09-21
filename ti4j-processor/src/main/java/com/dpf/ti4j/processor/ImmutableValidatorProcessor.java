720ff240ccfd61552dece9c732fecbaccbb916a6
Daniel Plaza Fernández <dpf220691@gmail.com>
2024-09-21 02:59:51 +0200

    feat: add reflections nad make it dynamic

--- ti4j-processor/src/main/java/com/dpf/ti4j/processor/ImmutableValidatorProcessor.java
+++ ti4j-processor/src/main/java/com/dpf/ti4j/processor/ImmutableValidatorProcessor.java
@@ -1,36 +1,57 @@
 package com.dpf.ti4j.processor;
 
 import com.dpf.ti4j.core.Immutable;
-import net.bytebuddy.agent.ByteBuddyAgent;
+import com.dpf.ti4j.core.ImmutableValidator;
 import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.agent.ByteBuddyAgent;
 import net.bytebuddy.asm.Advice;
+import net.bytebuddy.asm.Advice.OnMethodExit;
+import net.bytebuddy.asm.Advice.This;
 import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
 import net.bytebuddy.matcher.ElementMatchers;
+import org.reflections.Reflections;
+
+import java.util.Set;
 
 public class ImmutableValidatorProcessor {
 
-    public static void activateValidation(Class<?> clazz) {
-        System.out.println("Instalando ByteBuddy agent...");
+    public static void activateImmutableValidation() {
+
+        System.out.println("Installing ByteBuddy agent...");
+
         ByteBuddyAgent.install();
 
-        // Aplicar Advice a las clases que son anotadas con @Immutable
-        new ByteBuddy()
-                .redefine(clazz)  // Redefinir dinámicamente la clase Car
-                .visit(Advice.to(ConstructorAdvice.class).on(ElementMatchers.isConstructor()))  // Interceptar el constructor sin llamar a super
-                .make()
-                .load(clazz.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
+        Reflections reflections = new Reflections("");
+        Set<Class<?>> immutableClasses = reflections.getTypesAnnotatedWith(Immutable.class);
 
-        System.out.println("ByteBuddy agent instalado.");
-    }
+        for (Class<?> immutableClass : immutableClasses) {
+            try {
+                System.out.println("Transforming class: " + immutableClass.getName());
 
-    // Clase para validar el constructor
-    public static class ConstructorAdvice {
-        @Advice.OnMethodExit
-        public static void validate(@Advice.This Object instance) {
-            System.out.println("Validando instancia de: " + instance.getClass().getName());
-            if (instance.getClass().isAnnotationPresent(Immutable.class)) {
-                com.dpf.ti4j.core.ImmutableValidator.validate(instance);
+                new ByteBuddy()
+                        .redefine(immutableClass)
+                        .visit(Advice.to(ConstructorAdvice.class).on(ElementMatchers.isConstructor()))
+                        .make()
+                        .load(immutableClass.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
+
+            } catch (Exception e) {
+                System.out.println("Error transforming class: " + immutableClass.getName());
             }
         }
+
+        System.out.println("ByteBuddy agent installed successfully");
+    }
+
+    public static class ConstructorAdvice {
+
+        @OnMethodExit
+        public static void validate(@This Object instance) {
+
+            System.out.println("Validating: " + instance.getClass().getName());
+            if (instance.getClass().isAnnotationPresent(Immutable.class)) {
+                ImmutableValidator.validate(instance);
+            }
+        }
+
     }
 }
--- ti4j-test/src/main/java/com/dpf/ti4j/test/Main.java
+++ ti4j-test/src/main/java/com/dpf/ti4j/test/Main.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        ImmutableValidatorProcessor.activateValidation(Car.class);
+        ImmutableValidatorProcessor.activateImmutableValidation();
 
         System.out.println("Hello world!");
 
